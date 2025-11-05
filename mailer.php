48a4c131437b64b2f9b39244433272fad3fba152
S.M. Tahmid Hasan Shahria <smtahmidh@gmail.com>
2025-11-06 02:44:22 +0600

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "alaminorko70@gmail.com";
+        $recipient = "smtahmidh@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
